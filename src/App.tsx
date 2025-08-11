37731a49384a4e35ac07b68032298af0f10f99e7
Ivan Bosnic <ivan.bosnic.st@gmail.com>
2025-08-11 11:18:30 +0200

    Fix router basename

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import './App.css'
 function App() {
   return (
     <RentalDataProvider>
-      <BrowserRouter basename="/roi-calculator">
+      <BrowserRouter>
         <div className="container">
           <header>
             <div className="header-left">
